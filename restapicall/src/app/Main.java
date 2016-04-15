685ab2d336d726d3a94cd2e6302218b5b0231490
Artur Joshi <arturjoshi.mailbox@gmail.com>
2016-04-15 17:59:07 +0300

    Added REST API call Java example

---
+++ restapicall/src/app/Main.java
@@ -0,0 +1,32 @@
+package app;
+
+import com.sun.org.apache.xml.internal.security.utils.Base64;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://10.2.1.205:8080/kaaAdmin/rest/api/applications");
+            String encoding = Base64.encode("devuser:devuser123".getBytes());
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Authorization", "Basic " + encoding);
+
+            InputStream content = connection.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(content));
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
